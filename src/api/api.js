31f2afdd8c14d4551ccf32731d14f99b70187a42
LinnJS <justinslinn727@gmail.com>
2020-07-19 16:31:24 -0400

    Configure API routes

--- src/api/api.js
+++ src/api/api.js
@@ -1,21 +1,8 @@
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => {
-  res.json({message: 'ok'})
-})
-
-app.get('/todos', (req, res) => {
-  const todos = [
-    {id: 1, name: 'clean up', status: 'open'},
-    {id: 1, name: 'cook', status: 'done'}
-  ]
-  res.json({data: todos})
-})
-
-
-app.get('/todos/:todo', (req, res) => {
-  res.json({id: 1, name: 'clean up', status: 'open'})
-})
-
-module.exports = app
+module.exports.handler = (event, ctx, done) => {
+    done(null, {
+        statusCode: 200,
+        headers: {},
+        body: JSON.stringify({message: 'hello'})
+    })
+  }
+  
